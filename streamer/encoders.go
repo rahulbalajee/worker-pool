a340852ac14abae33f371b4b837e649525d14dc1
rahulbalajee <rahrkb4@gmail.com>
2025-07-29 22:17:00 +0530

    added ffmpeg

--- streamer/encoders.go
+++ streamer/encoders.go
@@ -1,5 +1,11 @@
 package streamer
 
+import (
+	"fmt"
+
+	"github.com/xfrr/goffmpeg/transcoder"
+)
+
 type Encoder interface {
 	EncodeToMP4(v *Video, baseFileName string) error
 }
@@ -7,5 +13,28 @@ type Encoder interface {
 type VideoEncoder struct{}
 
 func (ve *VideoEncoder) EncodeToMP4(v *Video, baseFileName string) error {
+	// Create a transcoder
+	trans := new(transcoder.Transcoder)
+
+	// Build the output path
+	outputPath := fmt.Sprintf("%s/%s", v.OutputDir, baseFileName)
+
+	// Initialise the transcoder
+	err := trans.Initialize(v.InputFile, outputPath)
+	if err != nil {
+		return err
+	}
+
+	// Set the codec
+	trans.MediaFile().SetVideoCodec("libx264")
+
+	// Start transcoding process
+	done := trans.Run(false)
+
+	err = <-done
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- streamer/go.mod
+++ streamer/go.mod
@@ -1,3 +1,5 @@
 module streamer
 
 go 1.24.1
+
+require github.com/xfrr/goffmpeg v1.0.0 // indirect
